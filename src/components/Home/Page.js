2bbdddb542f0f2830dae129d500743f8df73efcf
Ahmed Ibrahim <ib153507@gmail.com>
2022-01-21 18:33:38 +0200

    Add Icon and background

--- src/components/Home/Page.js
+++ src/components/Home/Page.js
@@ -1,7 +1,26 @@
 import React from "react";
 
-const Page = () => {
-  return <div>Page</div>;
+const Page = ({}) => {
+  return (
+    <div className="relative">
+      <div className="h-96 overflow-hidden">
+        <div className="w-full h-full">
+          <img
+            className="object-cover inline-block w-full h-full"
+            src="/assets/default-bg.jpg"
+            alt="Cover"
+          />
+        </div>
+        <div className="absolute bottom-4 left-4 z-10 w-16 h-16 cursor-pointer hover:opacity-90 hover:bg-zinc-50 p-1 rounded-lg">
+          <img
+            src="https://notion-emojis.s3-us-west-2.amazonaws.com/prod/svg-twitter/1f579-fe0f.svg"
+            alt="Icon"
+            className="object-cover inline-block w-full h-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
--- src/components/Home/Sidebar.js
+++ src/components/Home/Sidebar.js
@@ -4,7 +4,7 @@ const Sidebar = () => {
   return (
     <div className="w-full h-full">
       <div className="">
-        <button className="flex items-center justify-start text-left text-sm font-medium w-full px-3 hover:bg-gray-200 py-3">
+        <button className="flex items-center justify-start text-left text-sm font-medium w-full px-3 hover:bg-gray-200 py-3 text-stone-800">
           <i className="fas fa-plus mr-2"></i> <p>Add Page</p>
         </button>
       </div>
