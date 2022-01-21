e1dfe0cb0990fc1a2592189ad5db064330e19a2c
Ahmed Ibrahim <ib153507@gmail.com>
2022-01-21 18:15:43 +0200

    Add new page button

---
+++ src/components/Home/Sidebar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="w-full h-full">
+      <div className="">
+        <button className="flex items-center justify-start text-left text-sm font-medium w-full px-3 hover:bg-gray-200 py-3">
+          <i className="fas fa-plus mr-2"></i> <p>Add Page</p>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Page from "../components/Home/Page";
+import Sidebar from "../components/Home/Sidebar";
 
 const Home = () => {
-  return <div>hello home</div>;
+  return (
+    <div className="w-screen h-screen flex items-center justify-start">
+      <div className="col-span-3 h-full w-64 bg-stone-100">
+        <Sidebar />
+      </div>
+      <div className="col-span-9 bg-gray-50 h-full w-full">
+        <Page />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
